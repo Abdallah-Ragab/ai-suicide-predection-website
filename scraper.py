c13638e31f3b6a9cc4377c527a04d3652f8aafaa
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-05-06 00:32:47 +0300

    chore: Update Scraper class initialization
    The Scraper class initialization has been updated to remove the unused `url` parameter. This parameter was not being used in the class, so it has been removed to improve code clarity and maintainability.

--- scraper.py
+++ scraper.py
@@ -1,21 +1,39 @@
 from ntscraper import Nitter
 
 class Scraper:
-    tweet_limit = 50
+    tweet_limit = 5
     def __init__(self):
-        self.scraper = Nitter()
+        self.scraper = Nitter(instances=['https://nitter.esmailelbob.xyz'])
 
     def user(self, username):
         return self.scraper.get_profile_info(username)
 
     def tweets(self, username):
         try:
-            return self.scraper.get_tweets(username, mode='user', number=self.tweet_limit, language='en')
+            return self.scraper.get_tweets(username, mode='user', number=self.tweet_limit)
         except Exception as e:
             return str(e)
 
 
+class cli:
+    def __init__(self):
+        self.scraper = Scraper()
+        self.welcome()
+
+    def welcome(self):
+        print('Welcome to the Twitter Scraper CLI')
+        print('-----------------------------------')
+        self.prompt()
+
+    def prompt(self):
+        print('Enter a username to scrape (\'q\' to quit):')
+        username = input()
+        if username == 'q':
+            exit()
+        print('Scraping tweets...')
+        tweets = self.scraper.tweets(username)
+        print(tweets)
+        self.prompt()
 
 
-s = Scraper()
-print(s.user('elonmusk'))
+cli()
